459b23e51b729d1a500f9ad1b411e265817cdfa6
xkendx <63884610+xkendx@users.noreply.github.com>
2021-04-23 18:07:07 +0300

    feat: while-do-while

--- JavaDemos/src/loops/Main.java
+++ JavaDemos/src/loops/Main.java
@@ -5,12 +5,28 @@ public class Main {
     public static void main(String[] args) {
         
         // for loop
-        for (int i=1; i<10; i++) {
+        for (int i=1; i<10; i+=2) {
             System.out.println(i);
         }
+            System.out.println("-------------End of For Loop!!!-------------");
+            
+        // While loop
+        int i = 1;
+        while(i<10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("-------------End of While Loop!!!-------------");
 
-        // 
+        // do-while loop
+        int j = 1;
+        do{
+            System.out.println(j);
+            j+=2;
+
+        }while(j<10);
+        System.out.println("-------------End of Do-While Loop!!!-------------");
 
 
+        }
     }
-}
