e87578b4437777a8c99d98d1bc22c9b0773c365e
xkendx <63884610+xkendx@users.noreply.github.com>
2021-04-23 17:37:23 +0300

    feat: conditionals

---
+++ JavaDemos/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int number = 20;
+
+        if(number < 20){
+            System.out.println("Number lower than 20");
+        } else if (number > 20) {
+            System.out.println("Number higher than 20");
+        } else {
+            System.out.println("Number is equal to 20");
+        }
+    }
+
+
+}
