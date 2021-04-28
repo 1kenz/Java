1b9ee20d396946d45d1faa99293bbe02e0eae657
xkendx <63884610+xkendx@users.noreply.github.com>
2021-04-28 21:53:40 +0300

    feat: start oop2

--- JavaCamp/src/oopIntro/Product.java
+++ JavaCamp/src/oopIntro/Product.java
@@ -6,19 +6,24 @@ public class Product {
         System.out.println("First Constructor, Instance create  started!");
 
     }
-    
-    public Product(int id, String name, double unitPrice, String detail) {  // overloading
-        this();
-        this.id = id;
-        this.name = name;
-        this.unitPrice = unitPrice;
-        this.detail = detail;
-        System.out.println("Second Constructor, id number: " + id + " Instance created!");
-        
-    }
 
     int id;
     String name;
     double unitPrice;
     String detail;
+    double discount;
+    double unitPriceAfterDiscount;
+    
+    public Product(int id, String name, double unitPrice, String detail, double discount, double unitPriceAfterDiscount) {  // overloading
+        this();
+        this.id = id;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.detail = detail;
+        this.discount = discount;
+        System.out.println("Second Constructor, id number: " + id + " Instance created!");
+        
+    }
+
+   
 }
