cab2b93fd184ff023264b8dde533f631eed77d6d
xkendx <63884610+xkendx@users.noreply.github.com>
2021-04-29 02:37:09 +0300

    feat: methods

---
+++ JavaDemos/src/methods/Main.java
@@ -0,0 +1,26 @@
+package methods;
+
+public class Main {
+    public static void main(String[] args) {
+        findNumber();
+    }
+
+    public static void findNumber() {
+        int[] numbers = new int[]{1,2,3,7,5,9};
+        int findNumber = 4;
+        boolean isExist = false;
+
+        for (int number : numbers) {
+            if(number == findNumber){
+                isExist = true;
+                break;
+            } 
+        }
+        if(isExist){
+            System.out.println("Number is existed");
+        } else {
+
+            System.out.println("Number is NOT existed");
+        }
+    }
+}
