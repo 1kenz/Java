6003f6458aa9588fad286847034f48f4fb2b4f69
xkendx <63884610+xkendx@users.noreply.github.com>
2021-04-29 22:02:25 +0300

    fix: fix method

--- JavaDemos/src/methods2/Main.java
+++ JavaDemos/src/methods2/Main.java
@@ -1,10 +1,9 @@
 package methods2;
 
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Weather is very good today";
+        // String message = "Weather is very good today";
         // String newMessage = message.substring(0,2);
         String newMessage = getCity();
         System.out.println(newMessage);
