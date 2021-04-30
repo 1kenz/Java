dc64c48fe673d0efc22896c9d0b63c1cece0caea
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-01 00:09:51 +0300

    feat: refactor code

--- JavaDemos/src/classesWithAttributes/Main.java
+++ JavaDemos/src/classesWithAttributes/Main.java
@@ -3,11 +3,11 @@ package classesWithAttributes;
 public class Main {
     public static void main(String[] args) {
         Product product = new Product();
-        product.name = "Dell 7550 Laptop";
-        product.description = "Dell i7 6700HQ processor";
-        product.id = 1;
-        product.price = 7000;
-        product.stockAmount = 1;
+        product.setName("Dell 7550 Laptop"); = ;
+        product.setDescription("Dell i7 6700HQ processor");
+        product.setId(1);
+        product.setPrice(7000);
+        product.setStockAmount(1);
 
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
--- JavaDemos/src/classesWithAttributes/Product.java
+++ JavaDemos/src/classesWithAttributes/Product.java
@@ -1,9 +1,60 @@
 package classesWithAttributes;
 
 public class Product {
-    int id;
-    String name;
-    String description;
-    double price;
-    int stockAmount;
+    private int _id;
+    private String _name;
+    private String _description;
+    private double _price;
+    private int _stockAmount;
+    private String _code;
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int id) {
+        _id = id;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public String getDescription() {
+        return _description;
+    }
+
+    public void setDescription(String description) {
+        _description = description;
+    }
+
+    public double getPrice() {
+        return _price;
+    }
+
+    public void setPrice(double price) {
+        _price = price;
+    }
+
+    public int getStockAmount() {
+        return _stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        _stockAmount = stockAmount;
+    }
+
+    public String getCode() {
+        return _code;
+    }
+
+    public void setCode(String code) {
+        _code = code;
+    }
+
+
 }
