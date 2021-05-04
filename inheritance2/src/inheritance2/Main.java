382041d158b7a6ceb7efaa6ce7e3558ab06701d0
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-04 05:32:57 +0300

    feat: inheritance better code

--- inheritance2/src/inheritance2/Main.java
+++ inheritance2/src/inheritance2/Main.java
@@ -3,8 +3,12 @@ package inheritance2;
 public class Main {
 
 	public static void main(String[] args) {
-		LogManager logManager = new LogManager();
-		logManager.log(1);
+		// LogManager logManager = new LogManager();
+		// logManager.log(1);
+		
+		CustomerManager customerManager = new CustomerManager();
+		// customerManager.add(new EmailLogger());
+		customerManager.add(new DatabaseLogger());
 	}
 
 }
