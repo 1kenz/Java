5c670867d0d39b2a194bd1de6743df23edefac07
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-05 17:29:46 +0300

    feat: interfaces bad codes

---
+++ interfaces/src/interfaces/CustomerManager.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public class CustomerManager {
+	public void add(Customer customer) {
+		System.out.println("Customer added " + customer.getFirstName());
+		DatabaseLogger logger = new DatabaseLogger();
+		logger.log(customer.getFirstName() + " logged to DB");
+		
+	}
+	
+	public void delete(Customer customer) {
+		System.out.println("Customer deleted " + customer.getFirstName());
+		DatabaseLogger logger = new DatabaseLogger();
+		logger.log(customer.getFirstName() + " logged to DB");
+	}
+}
--- interfaces/src/interfaces/Main.java
+++ interfaces/src/interfaces/Main.java
@@ -3,7 +3,11 @@ package interfaces;
 public class Main {
 
 	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
 		
+		Customer ken = new Customer(1, "ken", "den");
+		
+		customerManager.add(ken);
 	}
 
 }
