2761c27bb3cf4636b8cfc76628136b50a66605c6
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-05 17:44:57 +0300

    feat: interfaces best pratice

--- interfaces/src/interfaces/Main.java
+++ interfaces/src/interfaces/Main.java
@@ -3,9 +3,12 @@ package interfaces;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Logger[] loggers = {new SmsLogger(), new EmailLogger(), new FileLogger()};
+		
 		// CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
 		// CustomerManager customerManager = new CustomerManager(new SmsLogger());
-		CustomerManager customerManager = new CustomerManager(new EmailLogger());
+		CustomerManager customerManager = new CustomerManager(loggers);
 
 		
 		Customer ken = new Customer(1, "ken", "den");
