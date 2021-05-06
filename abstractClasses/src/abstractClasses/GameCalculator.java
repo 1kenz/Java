b160ab9cbbbc5fa91e19e101d9b9cf6694c38562
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-06 23:52:59 +0300

    feat: bad code

---
+++ abstractClasses/src/abstractClasses/GameCalculator.java
@@ -0,0 +1,11 @@
+package abstractClasses;
+
+public class GameCalculator {
+	public void calculate() {
+		System.out.println("Your Score: 100");
+	}
+	
+	public void gameOver() {
+		System.out.println("Game over...");
+	}
+}
