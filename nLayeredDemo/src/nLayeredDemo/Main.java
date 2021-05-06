7e054c4f0ef160a1c06dc9e4f383b088dc2f3fc9
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-06 12:05:02 +0300

    feat: add outsource injection

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -2,6 +2,7 @@ package nLayeredDemo;
 
 import nLayeredDemo.business.abstructs.ProductService;
 import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
 import nLayeredDemo.dataAccess.concretes.AbcProductDao;
 // import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
 import nLayeredDemo.entities.concretes.Product;
@@ -12,7 +13,7 @@ public class Main {
 		
 		// TODO: Its solved by Spring IoC
 		// ProductService productService = new ProductManager(new HibernateProductDao());
-		ProductService productService = new ProductManager(new AbcProductDao());
+		ProductService productService = new ProductManager(new AbcProductDao(), new JLoggerManagerAdapter());
 		
 		Product product = new Product(1,2,"Apple", 12,50);
 		productService.add(product);
