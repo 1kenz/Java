e34c520256e3296c1d3df5c697c16572e953f96f
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-06 11:42:12 +0300

    feat: update

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -1,9 +1,21 @@
 package nLayeredDemo;
 
+import nLayeredDemo.business.abstructs.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.dataAccess.concretes.AbcProductDao;
+// import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		// TODO: Its solved by Spring IoC
+		// ProductService productService = new ProductManager(new HibernateProductDao());
+		ProductService productService = new ProductManager(new AbcProductDao());
+		
+		Product product = new Product(1,2,"Apple", 12,50);
+		productService.add(product);
 
 	}
 
