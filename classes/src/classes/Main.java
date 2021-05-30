5c9c03d07fb0a8ecbb817e8011d6dcc515e82a9c
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-30 03:01:37 +0300

    feat: classes reference types

--- classes/src/classes/Main.java
+++ classes/src/classes/Main.java
@@ -9,6 +9,19 @@ public class Main {
 		customerManager.Delete();
 		customerManager.Read();
 		customerManager.Update();
-	}
 	
+		// primitive types - value -- stack
+		int number1 = 10;
+		int number2 = 25;
+		number2 = number1;
+		number1 = 55;
+		System.out.println(number2);
+		
+		// reference types -- stack -- heap (reference)
+		int[] numbers3 = {1,2,3,5};
+		int[] numbers4 = new int[] {5,7,9,11};
+		numbers4 = numbers3;
+		numbers3[1] = 15;
+		System.out.println(numbers4[1]);
+	}
 }
