474954a966dfc1c3a63ef750a29f75c3d6a8df3f
xkendx <63884610+xkendx@users.noreply.github.com>
2021-05-31 03:24:17 +0300

    feat: polymorphism

---
+++ polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,22 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// EmailLogger logger = new EmailLogger();
+		// logger.Log("Logged...");
+
+		// BaseLogger[] loggers = new BaseLogger[] {new FileLogger(), new EmailLogger(), new DatabaseLogger()};
+		// for (BaseLogger logger : loggers) {
+		// logger.Log("Log message");
+		
+		
+		// }
+	
+		CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+		customerManager.Add();
+	
+	}
+
+}
