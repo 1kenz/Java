c66ab68baa49425cf359709504fbbb4df3185256
xkendx <63884610+xkendx@users.noreply.github.com>
2021-06-16 01:25:19 +0300

    feat: add assignment-2

---
+++ Assignment2-coffeeShop/readme.md
@@ -0,0 +1,3 @@
+# Coffee Shop Management System
+
+![Senaryo](https://github.com/xkendx/Java/blob/main/Assignments/Assingment-2(CoffeeShop)/CoffeeShop/images/Senaryo.jpg)
---
+++ Assignment2-coffeeShop/src/Entities/Customer.java
@@ -0,0 +1,16 @@
+package Entities;
+
+import java.util.Date;
+
+import Abstract.EntitiesServices;
+
+public class Customer implements EntitiesServices{
+
+	public int Id;
+	public String firstName;
+	public String lastName;
+	public Date dateOfBirth;
+	public String nationalityId;
+	
+	
+}
