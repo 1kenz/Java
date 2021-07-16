d1d3341fe054abd5645edebe2e6302f2e13c63c3
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-17 01:47:08 +0300

    feat: variables

--- learning/intellij-idea/javaDemos/variables/src/Main.java
+++ learning/intellij-idea/javaDemos/variables/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // case sensitive
+        // variable names define camelCase
+
+        int number = 10;
+        String message = "My class have students. ";
+
+        System.out.println(message + number);
+        System.out.println("My class have 11 students.");
+        System.out.println("My class have 11 students.");
+        System.out.println("My class have 11 students.");
     }
 }
