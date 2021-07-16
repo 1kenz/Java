20e6c70e2c9a4d4a870fb2f2a35c571bcee5d4f4
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-17 02:23:19 +0300

    feat: while and do-while loop

---
+++ learning/intellij-idea/javaDemos/loopDemo/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // for loop
+        for (int i=1; i<10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("For loop end!");
+
+        for (int i=1; i<10; i +=2) {
+            System.out.println(i);
+        }
+        System.out.println("------------------");
+
+        // while
+        int i = 1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While loop end!");
+        System.out.println("-------------------");
+        // do-while
+        int j = 1;
+
+        do {
+            System.out.println(j);
+            j += 2;
+        }
+        while (i<10);
+        System.out.println("Do-While loop end!");
+
+    }
+}
