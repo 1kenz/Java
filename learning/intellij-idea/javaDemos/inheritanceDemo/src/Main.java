d271d51f2c91b2cad8050a178bca2ef1ffd6c45d
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-21 02:45:10 +0300

    feat: inheritance

---
+++ learning/intellij-idea/javaDemos/inheritanceDemo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // TeacherCreditManager teacherCreditManager = new TeacherCreditManager ();
+        // teacherCreditManager.Calculate ();
+
+        CreditUI creditUI = new CreditUI ();
+        creditUI.CreditCalculate (new TeacherCreditManager ());
+        creditUI.CreditCalculate (new ArmyCreditManager ());
+    }
+}
