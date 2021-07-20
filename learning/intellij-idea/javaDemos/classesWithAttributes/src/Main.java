ac14afba820087edb23a5923185080bb560910d6
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-21 02:14:00 +0300

    feat: add encapsulation getters and setters

---
+++ learning/intellij-idea/javaDemos/classesWithAttributes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Dell Laptop");
+        product.setPrice(10000);
+        product.setStockAmount(7);
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+
+        // System.out.println(product.name);
+    }
+}
