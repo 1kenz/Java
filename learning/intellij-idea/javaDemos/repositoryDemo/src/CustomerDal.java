1e59e5984a0025b7014fb80981ace60e0bd43e43
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-25 02:18:17 +0300

    feat: generics

---
+++ learning/intellij-idea/javaDemos/repositoryDemo/src/CustomerDal.java
@@ -0,0 +1,22 @@
+public class CustomerDal implements IEntityRepository<Customer> {
+
+    @Override
+    public void add(Customer entity) {
+
+    }
+
+    @Override
+    public void read(Customer entity) {
+
+    }
+
+    @Override
+    public void delete(Customer entity) {
+
+    }
+
+    @Override
+    public void update(Customer entity) {
+
+    }
+}
---
+++ learning/intellij-idea/javaDemos/repositoryDemo/src/ProductDal.java
@@ -0,0 +1,24 @@
+public class ProductDal implements IEntityRepository<Product> {
+
+    @Override
+    public void add(Product entity) {
+
+    }
+
+    @Override
+    public void read(Product entity) {
+
+    }
+
+    @Override
+    public void delete(Product entity) {
+
+    }
+
+    @Override
+    public void update(Product entity) {
+
+    }
+}
+
+// generic constraints (kısıt)
