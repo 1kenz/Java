990faafb6f9bf85b3bdc70216fa2e67c3afb77c5
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-24 15:29:03 +0300

    feat: static

---
+++ learning/intellij-idea/javaDemos/staticDemo/src/ProductManager.java
@@ -0,0 +1,10 @@
+public class ProductManager {
+    public void add(Product product){
+        ProductValidator validator = new ProductValidator ();
+        if (validator.isValid (product)){
+            System.out.println ( "Added." );
+        } else {
+            System.out.println ( "Check your product info" );
+        }
+    }
+}
