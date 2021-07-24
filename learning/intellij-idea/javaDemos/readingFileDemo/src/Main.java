dd5374b7855f380140fd5c6ad1bc0423a35e12a8
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-24 18:26:02 +0300

    feat: exception handling

---
+++ learning/intellij-idea/javaDemos/readingFileDemo/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader reader = null;
+        int total = 0;
+
+        try {
+            reader = new BufferedReader (new FileReader ("D:\\__coding\\Java\\learning\\intellij-idea\\javaDemos\\readingFileDemo\\src\\numbers.txt"));
+            String line = null;
+            while((line = reader.readLine ()) != null) {
+                total += Integer.valueOf (line);
+
+            }
+            System.out.println ( "Total = " + total );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace ();
+        } catch (Exception e) {  // base class
+            e.printStackTrace ( );
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e){
+
+            }
+
+        }
+
+    }
+}
