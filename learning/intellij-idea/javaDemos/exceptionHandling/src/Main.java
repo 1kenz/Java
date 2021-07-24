87bb17838c317f3960bbfa0d3b8c3009801cb6a4
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-24 18:07:11 +0300

    feat: exception hierarchy

---
+++ learning/intellij-idea/javaDemos/exceptionHandling/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+            int[] numbers = new int[] {1,2,3};
+            System.out.println ( numbers[5] );
+        } catch (StringIndexOutOfBoundsException exception){
+            System.out.println ( "Error rising: "+ exception );
+        } catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println ( "Error rising: "+ exception );
+        } catch (Exception exception){
+            System.out.println ( "Logging: "+ exception );
+        }
+        finally {
+            System.out.println ( "This codes always running ..." );  // for example log operations
+        }
+    }
+}
