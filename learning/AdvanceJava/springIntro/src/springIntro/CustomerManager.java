d1c2677f725077ee8e05eaa6ec2573189f0a9f18
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-31 01:16:24 +0300

    feat: setter injection

--- learning/AdvanceJava/springIntro/src/springIntro/CustomerManager.java
+++ learning/AdvanceJava/springIntro/src/springIntro/CustomerManager.java
@@ -3,17 +3,23 @@ package springIntro;
 public class CustomerManager implements ICustomerService {
 	
 	// dependency injection
-	
 	private ICustomerDal customerDal;
 	
-	public CustomerManager(ICustomerDal customerDal) {
+	// constructor injection *** preferred
+	// public CustomerManager(ICustomerDal customerDal) {
+	// 	this.customerDal = customerDal;
+	// }
+	
+	// setter injection -- not preferred
+	public void setCustomerDal(ICustomerDal customerDal) {
 		this.customerDal = customerDal;
 	}
-	
+
 	public void add() {
 		// business logic
 		
 		// CustomerDal customerDal = new CustomerDal();
+		customerDal.add();
 		
 		
 	}
