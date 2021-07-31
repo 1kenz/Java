7c2b4e604fe7c86c0afb418229fab2f8d2f2accd
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-31 03:51:39 +0300

    feat: exist query

--- learning/AdvanceJava/hibernateDemo/src/com/hibernatedemo/Main.java
+++ learning/AdvanceJava/hibernateDemo/src/com/hibernatedemo/Main.java
@@ -19,7 +19,7 @@ public class Main {
 		try {
 			session.beginTransaction();
 			// Select * from city
-			List<City> cities = session.createQuery("from City c where c.countryCode='TUR' OR c.countryCode='USA' ").getResultList();
+			List<City> cities = session.createQuery("from City c where c.name LIKE '%stan%'").getResultList();
 					
 			for(City city : cities) {
 				System.out.println(city.getName());
